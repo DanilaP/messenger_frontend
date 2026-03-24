6a6527df2238291f51a11c36f1a5fd7347cf9a6c
Danila Platonov <danilaplatonov209@gmail.com>
2026-03-24 15:48:55 +0300

    initial

--- src/stores/user/user.ts
+++ src/stores/user/user.ts
@@ -8,7 +8,7 @@ const stateInitial: UserStore = {
     user: null,
 }
 
-function reducer(state = stateInitial, action: { type: string, payload: any }) {
+function reducer(state = stateInitial, action: { type: string, payload: IUser }) {
     switch(action.type) {
         case "SET_USER": return { ...state, user: action.payload }
         default: return state;
