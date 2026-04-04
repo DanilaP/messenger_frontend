7cf9a4800b11742f301f8a23a0e01373ae95fa21
Danila Platonov <danilaplatonov209@gmail.com>
2026-04-04 12:30:43 +0300

    initial

--- src/components/pages/dialogs/components/dialog/components/header/header.tsx
+++ src/components/pages/dialogs/components/dialog/components/header/header.tsx
@@ -1,7 +1,9 @@
-import type { IOpponent } from '../../../../../../../models/dialogs/dialogs-interface';
 import { MdSearch } from "react-icons/md";
 import { memo } from 'react';
-
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { Dropdown, type MenuProps } from "antd";
+import { MdAttachFile, MdDelete  } from "react-icons/md";
+import type { IOpponent } from '../../../../../../../models/dialogs/dialogs-interface';
 import './header.scss';
 
 interface IDialogHeaderProps {
@@ -9,6 +11,31 @@ interface IDialogHeaderProps {
 }
 
 const DialogHeader = memo(({ opponent }: IDialogHeaderProps) => {
+
+    const items: MenuProps['items'] = [
+        {
+            label: 'Медиа',
+            key: '1',
+            icon: <MdAttachFile />,
+        },
+        {
+            label: 'Удалить диалог',
+            key: '2',
+            icon: <MdDelete  />,
+        },
+    ];
+
+    const handleMenuClick: MenuProps['onClick'] = (info) => {
+        info.domEvent.stopPropagation();
+        const { key } = info;
+        if (key === '1') {
+            console.log('Просмотр медиа');
+        }
+        else if (key === '2') {
+            console.log('Удаление диалога');
+        }
+    };
+
     return (
         <div className='dialog-header'>
             <div className="dialog-image-wrapper">
@@ -16,7 +43,10 @@ const DialogHeader = memo(({ opponent }: IDialogHeaderProps) => {
             </div>
             <div className="dialog-name">{opponent.name} {opponent.surname}</div>
             <div className="dialog-settings">
-                <MdSearch />
+                <MdSearch className="icon" />
+                <Dropdown menu={{ items, onClick: handleMenuClick }} trigger={['click']}>
+                    <BsThreeDotsVertical className="icon" />
+                </Dropdown>
             </div>
         </div>
     );
