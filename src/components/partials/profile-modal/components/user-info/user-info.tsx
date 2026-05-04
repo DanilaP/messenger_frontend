eb022174a14bc3daa12f271e9fb2da8958c22adc
Danila Platonov <danilaplatonov209@gmail.com>
2026-05-04 09:55:45 +0300

    add react-crop-image and create image-cropper

--- src/components/partials/profile-modal/components/user-info/user-info.tsx
+++ src/components/partials/profile-modal/components/user-info/user-info.tsx
@@ -1,35 +1,47 @@
 import { Input, Upload } from "antd";
 import { changeUserAvatar } from "../../../../../models/user-profile/user-profile-api";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { rootStore, type RootState } from "../../../../../stores/root/root";
 import type { IChangeUserAvatarResponse, IUserProfileInfo } from "../../../../../models/user-profile/user-profile-interface";
+import Cropper from "../../../cropper/cropper";
 import "./user-info.scss";
+
 
 export interface IUserInfoProps {
     userInfo: IUserProfileInfo;
 }
 
 const UserInfo = ({ userInfo }: IUserInfoProps) => {
-	
 	const [avatarUrl, setAvatarUrl] = useState<string>(userInfo.avatar);
+	const [cropModalOpen, setCropModalOpen] = useState(false);
+	const [selectedFile, setSelectedFile] = useState<File | null>(null);
+	const user = useSelector((state: RootState) => state.user.user);
 
-	const handleBeforeUpload = async (file: File) => {
+	const handleBeforeUpload = (file: File) => {
+		setSelectedFile(file);
+		setCropModalOpen(true);
+		return false;
+	};
+
+	const handleSaveCropped = async (croppedFile: File) => {
 		const formData = new FormData();
-		formData.append("files", file);
+		formData.append("files", croppedFile);
 
 		try {
-			changeUserAvatar(formData)
-				.then((res: IChangeUserAvatarResponse) => {
-					setAvatarUrl(res.data.avatar);
-					console.log(res);
-				})
-				.catch(error => {
-					console.error(error);
-				});
-		} 
-		catch (error) {
+			const res: IChangeUserAvatarResponse = await changeUserAvatar(formData);
+			setAvatarUrl(res.data.avatar);
+			setCropModalOpen(false);
+			setSelectedFile(null);
+			rootStore.dispatch({ type: "SET_USER", payload: { ...user, avatar: res.data.avatar } });
+		} catch (error) {
 			console.error("Ошибка при отправке файла", error);
 		}
-		return false;
+	};
+
+	const handleCancelCropper = () => {
+		setCropModalOpen(false);
+		setSelectedFile(null);
 	};
 
 	return (
@@ -56,6 +68,13 @@ const UserInfo = ({ userInfo }: IUserInfoProps) => {
 					<Input defaultValue={ userInfo.date_of_birth } />
 				</div>
 			</div>
+			<Cropper
+				open={ cropModalOpen }
+				file={ selectedFile }
+				onClose={ handleCancelCropper }
+				onSave={ handleSaveCropped }
+				aspect={ 1 }
+			/>
 		</div>
 	);
 };
